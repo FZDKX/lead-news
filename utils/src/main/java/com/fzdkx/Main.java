ba25e1d9eb9a92a3efc0da799273c632d30e11f5
发着呆看星 <2862219036@qq.com>
2024-02-02 23:10:32 +0800

    项目构建

---
+++ utils/src/main/java/com/fzdkx/Main.java
@@ -0,0 +1,11 @@
+package com.fzdkx;
+
+/**
+ * @author 发着呆看星
+ * @create 2024/2/2
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
